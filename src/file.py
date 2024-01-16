6f8c8a1cb14e8607cf36a64bd2543823f4f8d027
Jared Wenerd <wenerd87@gmail.com>
2024-01-15 23:28:10 -0700

    Write readme to output

--- src/file.py
+++ src/file.py
@@ -17,17 +17,19 @@ def file_hash(file):
 	return hash
 
 class Output:
+	readme_written = False
+
 	def __init__(self, file, records, extra_meta = {}):
 		self.file = file
-		self.extra_meta = extra_meta
 		self.mapping = Mapping(file, records)
 		self.len = len(records)
+		self.meta = self.get_meta(extra_meta)
 
-	def get_meta(self):
+	def get_meta(self, extra_meta = {}):
 		meta = { 'fetched': self.len, 'time':  datetime.now().isoformat() }
 		gh_meta = {k: v for k, v in os.environ.items() if k.lower() in GITHUB_META_KEYS}
 		meta.update(gh_meta)
-		meta.update(self.extra_meta)
+		meta.update(extra_meta)
 		return meta
 
 	def write_files(self):
@@ -36,7 +38,6 @@ class Output:
 		hash_before = file_hash(csv_file)
 		with open(csv_file, 'w', newline='') as csvfile:
 			writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-
 			rows = [self.mapping.columns] + self.mapping.get_rows()
 			writer.writerows(rows)
 
@@ -49,9 +50,26 @@ class Output:
 
 	def _write_meta(self):
 		with open('output/.' + self.file + '_meta', 'w', newline='') as metafile:
-			meta = self.get_meta()
-			keys = list(meta.keys())
+			keys = list(self.meta.keys())
 			keys.sort()
 			for key in keys:
-				value = meta.get(key,'')
+				value = self.meta.get(key,'')
 				metafile.writelines(key + "=" + str(value) + "\n")
+
+		self._write_readme()
+
+	def _write_readme(self):
+		if Output.readme_written:
+			# only doe this once per run
+			return
+
+		with open('output/README.md', 'w') as file:
+			print('writing readme')
+			content = None
+			if 'GITHUB_RUN_ID' in self.meta:
+				action_link = "https://github.com/" + self.meta['GITHUB_REPOSITORY'] + '/actions/runs/' + self.meta['GITHUB_RUN_ID']
+				content = '## ⚙️ [Action](' + action_link + ')'
+			if content is None:
+				content = 'manual'
+			file.write(content)
+			Output.readme_written = True
