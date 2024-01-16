13b60d5fda5890e0742d6555638ebdd6ba365e66
Jared Wenerd <wenerd87@gmail.com>
2024-01-16 12:25:06 -0700

    Update meta

--- src/file.py
+++ src/file.py
@@ -1,91 +1,97 @@
 import csv
 import os.path
+import subprocess
 import shutil
 from .mapping import Mapping
 from datetime import datetime
 from .append import filter_based_on_existing
-from .util import file_hash, write_file
+from .util import file_hash, write_file, output_path, is_github, github_meta
 
-GITHUB_META_KEYS = ['actor', 'event_name', 'job', 'ref_name', 'run_attempt', 'repository', 'run_id', 'run_number', 'sha', 'workflow']
-GITHUB_META_KEYS = tuple(['github_' + v for v in GITHUB_META_KEYS])
+
+
 
 class Output:
 	readme_written = False
 
 	def __init__(self, file, records, extra_meta = {}):
 		self.file = file
+		self.is_history = file == 'history'
 		self.mapping = Mapping(file, records)
 		self.fetched_len = len(records)
-		self.meta = self.get_meta(extra_meta)
-		self.csv_file = 'output/' + self.file + '.csv'
+		self.csv_file = self.file + '.csv'
+		self.csv_file_with_path = output_path(self.csv_file)
+		self.hash_before = file_hash(self.csv_file)
+		self.extra_meta = extra_meta
 
-	def get_meta(self, extra_meta = {}):
-		meta = { 'fetched': self.fetched_len, 'time':  datetime.now().isoformat() }
-		gh_meta = {k: v for k, v in os.environ.items() if k.lower() in GITHUB_META_KEYS}
-		meta.update(gh_meta)
-		meta.update(extra_meta)
+
+	def get_meta(self):
+		meta = { 'fetched_length': self.fetched_len, 'fetch_time':  datetime.now().isoformat() }
+		meta.update(github_meta())
+		meta.update(self.extra_meta)
+
+		file_meta_cmd = f"(wc -l {self.csv_file_with_path} && du -h {self.csv_file_with_path}) | xargs -n2 | cut -d ' ' -f 1"
+		file_lines, file_size = subprocess.check_output([file_meta_cmd], shell=True).decode().split()
+		meta.update({ 'file_lines': file_lines, 'file_size': file_size })
 		return meta
 
 	def write_csv(self):
-		with open('output/' + self.file + '.csv', 'w', newline='') as csvfile:
+		with open(self.csv_file_with_path, 'w', newline='') as csvfile:
 			writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
 			writer.writerows([self.mapping.columns] + self.rows)
 
 	def append_to_new(self):
-		with open(self.csv_file + '.old', 'r') as old_file, open(self.csv_file , 'a') as wr:
+		prev_file = f'{self.csv_file_with_path}.old'
+		with open(prev_file, 'r') as old_file, open(self.csv_file_with_path , 'a+') as new_file:
 			for index, line in enumerate(old_file):
 					if index > 0: # skip header
-						wr.write(line)
-		os.unlink(self.csv_file + '.old')
+						new_file.write(line)
+		os.unlink(prev_file)
 
 	def write_history_file(self):
-		filtered_rows = filter_based_on_existing(self.csv_file, self.rows)
+		filtered_rows = filter_based_on_existing(self.csv_file_with_path, self.rows)
 
 		if filtered_rows is None or len(filtered_rows) == 0:
 			print('no updates to history')
 			return
 
-		shutil.move(self.csv_file, self.csv_file + '.old')
+		shutil.move(self.csv_file_with_path, f'{self.csv_file_with_path}.old')
 
 		self.rows = filtered_rows
 		self.write_csv()
 		self.append_to_new()
 		self._log_write()
 
+
 	def write_files(self):
-		history = self.file == 'history'
-
-		csv_file = 'output/' + self.file + '.csv'
-		hash_before = file_hash(csv_file)
-
 		self.rows = self.mapping.get_rows()
-		if history:
+		if self.is_history:
 			self.write_history_file()
 		else:
 			self.write_csv()
 
-		if hash_before == file_hash(csv_file):
-			if not history:
+		if self.hash_before == file_hash(self.csv_file):
+			if not self.is_history:
 				print('no updates to ' + self.file)
 			return
 
 		self._write_meta()
-		if not history:
+		if not self.is_history:
 			self._log_write()
 
 	def _write_meta(self):
-		write_file('output/meta/' + self.file, self.meta)
+		self.meta = self.get_meta()
+		write_file(f'meta/{self.file}', self.get_meta())
 		self._write_readme()
 
 	def _write_readme(self):
 		if Output.readme_written:  # only doe this once per run
 			return
 		content = 'manual runs 🙃'
-		if 'GITHUB_RUN_ID' in self.meta:
-			action_link = "https://github.com/" + self.meta['GITHUB_REPOSITORY'] + '/actions/runs/' + self.meta['GITHUB_RUN_ID']
-			content = '## ⚙️ [Action](' + action_link + ')'
+		if is_github():
+			action_href = f"https://github.com/{self.meta['github_repository']}/actions/runs/{self.meta['github_run_id']}"
+			content = '## ⚙️ [Action](' + action_href + ')'
 
-		write_file('output/README.md', content)
+		write_file('README.md', content)
 		Output.readme_written = True
 
 	def _log_write(self):
