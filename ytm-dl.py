822822ec06d2ec43d2401e329d05dfd5a07011df
Jared Wenerd <wenerd87@gmail.com>
2024-01-22 07:36:05 -0700

    Search suggest

--- ytm-dl.py
+++ ytm-dl.py
@@ -1,10 +1,14 @@
 import sys
 import concurrent.futures
 import resource
+import string
+import yaml
 
-from src.api import ApiMethod
-from src.output import Output
+from src.api import ApiMethod, suggest_search
+from src.output import Output, write_output_yaml
 from src.meta import write_meta
+from src.util import write_output_file
+
 
 def ytmusic_to_file(file):
 	records, meta = ApiMethod(f'get_{file}').perform()
@@ -12,6 +16,13 @@ def ytmusic_to_file(file):
 	if result is None:
 		return
 	return result
+
+def write_search_suggest():
+	with concurrent.futures.ThreadPoolExecutor() as executor:
+		search_results = list(executor.map(lambda l: suggest_search(l), list(string.ascii_lowercase)))
+	write_output_yaml('search/suggest_by_letter', {row[0]: row[1] for row in search_results})
+	print('created suggest_by_letter')
+	return 'search/suggest_by_letter'
 
 def do_updates(option):
 	if not option in ['all', 'frequent']:
@@ -23,9 +34,12 @@ def do_updates(option):
 	if option == 'all':
 		files = files +  ['library_subscriptions', 'library_artists', 'library_albums']
 
-	print('starting ' + option + ' updates')
+	files_written = []
 	with concurrent.futures.ThreadPoolExecutor() as executor:
-		files_written = list(executor.map(lambda file: ytmusic_to_file(file), files))
+		files_written += list(executor.map(lambda file: ytmusic_to_file(file), files))
+
+	if option == 'all':
+		files_written += write_search_suggest()
 
 	files_written = set(filter(None, files_written))
 	output_updates = len(files_written) > 0
